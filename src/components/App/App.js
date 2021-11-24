0b4753a52e8841f3681f0c3a170ca4e165811726
Monzikov Maxim <monzikovm@yandex.ru>
2021-11-25 00:13:41 +0300

    feat: CRA

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     У тебя получится!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <div className="header container">
+        <img className="header__logo" src={logo} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
