e92fbe5b34806af50ffca8da85b610d419d3e6fb
Monzikov Maxim <monzikovm@yandex.ru>
2021-11-30 22:36:50 +0300

    feat: adaptive

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import './Main.css';
 import Promo from '../Main/Promo/Promo';
 import AboutProject from '../Main/AboutProject/AboutProject';
 import Techs from '../Main/Techs/Techs';
+import AboutMe from '../Main/AboutMe/AboutMe';
 import Portfolio from '../Main/Portfolio/Portfolio';
 
 function Main() {
@@ -11,6 +12,7 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
       <Portfolio />
     </div>
   )
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,43 +1,29 @@
 import React from 'react';
 import './Portfolio.css'
-import photo from '../../../images/photo.jpg';
-import arrow from '../../../images/arrow.svg';
 
 function Portfolio() {
   return (
     <section className="portfolio">
       <div className="portfolio__container">
-        <div className="portfolio__title-box">
-          <h2 className="portfolio__title">Студент</h2>
-        </div>
-        <article className="portfolio__data">
-        <div className="portfolio__box">
-          <h2 className="portfolio__name">Максим</h2>
-          <p className="portfolio__about">Фронтенд-разработчик, 27 лет</p>
-          <p className="portfolio__bio">Я родился в Вологде, но живу в Санкт-Петербурге , закончил факультет экономики МУБиНТ. Я люблю слушать музыку и увлекаюсь кинематографом. Недавно начал кодить. С 
-          2015 года работал в компании «СКБ Контур». После того, как прошёл курс по 
-          веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
-          </p>
-          <a href="https://vk.com/monzikovm" class="portfolio__link">VK</a>
-          <a href="https://github.com/MonzikovMaxim" class="portfolio__link">Github</a>
-        </div>
-          <img className="portfolio__photo" src={photo} alt="моё фото"></img>
-        </article>
-        <div className="portfolio__block">
           <div className="portfolio__subtitle">Портфолио</div>
           <div className="portfolio__site-box">
-            <a href="https://practicum.yandex.ru/web/" target="_blank" rel="noreferrer" className="portfolio__site-link">Статичный сайт</a>
-            <img className="portfolio__arrow" src={arrow} alt="стрелочка"></img>
+            <a href="https://practicum.yandex.ru/web/" target="_blank" rel="noreferrer" className="portfolio__site-link">
+            <p>Статичный сайт</p>
+            <p>↗</p>
+            </a>
           </div>
           <div className="portfolio__site-box">
-            <a href="https://monzikovmaxim.github.io/russian-travel/" rel="noreferrer" target="_blank" className="portfolio__site-link">Адаптивный сайт</a>
-            <img className="portfolio__arrow" src={arrow} alt="стрелочка"></img>
+            <a href="https://monzikovmaxim.github.io/russian-travel/" rel="noreferrer" target="_blank" className="portfolio__site-link">
+            <p>Адаптивный сайт</p>
+            <p>↗</p>
+            </a>
           </div>
           <div className="portfolio__site-box">
-            <a href="https://thisismesto.students.nomoredomains.monster"  rel="noreferrer" target="_blank" className="portfolio__site-link">Одностраничное приложение</a>
-            <img className="portfolio__arrow" src={arrow} alt="стрелочка"></img>
+            <a href="https://thisismesto.students.nomoredomains.monster"  rel="noreferrer" target="_blank" className="portfolio__site-link">
+            <p>Одностраничное приложение</p>
+            <p>↗</p>
+            </a>
           </div>
-        </div>
       </div>
     </section>
   )
